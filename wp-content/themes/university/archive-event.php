7ce15dc6b7746bbef61924eb19c51e30d7a9cf1a
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-13 10:49:40 +0700

    add ACF event date

--- wp-content/themes/university/archive-event.php
+++ wp-content/themes/university/archive-event.php
@@ -20,8 +20,12 @@ get_header();
       the_post(); ?>
       <div class="event-summary">
         <a class="event-summary__date t-center" href="<?php echo get_permalink(); ?>">
-          <span class="event-summary__month"><?php the_time('M'); ?></span>
-          <span class="event-summary__day"><?php the_time('d'); ?></span>
+          <?php
+            // Load field value.
+            $date = new DateTime(get_field( 'event_time' ));
+          ?>
+          <span class="event-summary__month"><?php echo esc_html($date->format( 'M' )); ?></span>
+          <span class="event-summary__day"><?php echo esc_html($date->format( 'd' )); ?></span>
         </a>
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h5>
