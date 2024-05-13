26090d3cc234151d27646371f9f4f2adef2f3c1e
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-13 15:57:12 +0700

    short by time

--- wp-content/themes/university/front-page.php
+++ wp-content/themes/university/front-page.php
@@ -13,9 +13,11 @@
       <?php
         // The Query.
         $eventHomepage = new WP_Query( array( 
-          'posts_per_page' => 2,
-          'post_type' => 'event',
-          'order' => 'DESC',
+          'posts_per_page'=> 2,
+          'post_type'     => 'event',
+          'meta_key'      => 'event_time',
+          'orderby'       => 'meta_value_num',
+          'order'         => 'DESC',
         ) );
 
         // The Loop.
@@ -26,8 +28,12 @@
             ?>
               <div class="event-summary">
                 <a class="event-summary__date t-center" href="<?php echo get_permalink(); ?>">
-                  <span class="event-summary__month"><?php the_time('M'); ?></span>
-                  <span class="event-summary__day"><?php the_time('d'); ?></span>
+                  <?php
+                    // Load field value.
+                    $date = new DateTime(get_field( 'event_time' ));
+                  ?>
+                  <span class="event-summary__month"><?php echo esc_html($date->format( 'M' )); ?></span>
+                  <span class="event-summary__day"><?php echo esc_html($date->format( 'd' )); ?></span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h5>
